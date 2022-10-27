b6acc1133bacd357f16aefcce01996b474b59521
lhuisruiz <84159346+lhuisruiz@users.noreply.github.com>
2022-10-27 00:29:35 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,44 +1,51 @@
-// Modal error
-setTimeout(function () {
-Swal.fire({
-    position: 'top-end',
-    timer: 5000,
-    icon: 'error',
-    title: 'Permitir Para Ver Fotos',
-    showConfirmButton: false,
-    timer: 5000,
+const evento = document.getElementById('send')
+const enviarFormulario =() => {
 
-});
-}, 1000 * 3);
-
-// anuncio slider
-let slider = document.querySelector(".slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
-let contador = 1;
-let width = sliderIndividual[0].clientWidth;
-let intervalo = 4000;
-
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
-
-setInterval(function(){
-    slides();
-},intervalo);
-
-
-
-function slides(){
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform .8s";
-    contador++;
-
-    if(contador == sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador=1;
-        },1500)
-    }
+        let numero = document.getElementById('numero').value;
+       
+        
+var win= window.open(`https://wa.me/54${numero}?text=App:%20 
+https://bit.ly/3f2nzZI`,'_blank');       
+//
 }
+evento.addEventListener('click', enviarFormulario)
 
+
+
+//Compartir
+const shareBtn = document.getElementById('shareBtn')
+shareBtn.addEventListener('click', event => {
+    if (navigator.share) {
+        //drive
+        navigator.share({
+            text: 'Please read this great article: ',
+            url: 'https://bit.ly/3f2nzZI'
+        }).then(() => {
+            console.log('Thanks for sharing!');
+        })
+            .catch((err) => console.error(err));
+    } else {
+
+        alert("The current browser does not support the share function. Please, manually share the link")
+    }
+});
+
+
+
+
+ function modal_05() {
+    Swal.fire({
+        html: '<h3 class="bm">Black Momets</h3>',
+        imageUrl: 'https://res.cloudinary.com/lhuis/image/upload/v1666754485/iconos/fkppmxoe9qs9xhggmzcc.png',
+       //tamaño
+        imageWidth: 300,
+        imageHeight: 300,
+        //cerrar
+        padding:'1rem',
+        showCloseButton:true,
+        showConfirmButton:false,
+        background:'linear-gradient(#000,#000)',
+        
+    })
+ }
+ 
